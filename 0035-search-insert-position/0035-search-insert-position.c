74a0bd2c55b4bd85467ac2a3e5a0ca8b05eb3138
Rohit Jayprakash Kharode <136696475+rohitkharode@users.noreply.github.com>
2026-07-09 12:58:39 +0530

    Time: 0 ms (100%), Space: 8.9 MB (70.3%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,23 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int high = numsSize - 1;
+    int low = 0;
+    while(low <= high)
+    { 
+        int mid = low + (high-low)/2;
+        if(nums[mid] == target)
+        {
+            return mid;
+        }
+        else if(nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+
+    }
+    return low;
+    
+}
